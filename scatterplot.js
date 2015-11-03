4806e49c39a907d463fbb47dc0ac8a99a7c750b8
brentzucker <brentzucker@gmail.com>
2015-11-03 09:58:38 -0500

    Color for categories and DoD displays site name and category

--- scatterplot.js
+++ scatterplot.js
@@ -10,7 +10,9 @@ function scatterPlot() {
 	var y = d3.scale.linear()
 	    .range([height, 0]);
 
-	var color = d3.scale.category10();
+	// fill color
+	var cValue = function(d) { return d.main_category; },
+		color = d3.scale.category20();
 
 	var xAxis = d3.svg.axis()
 	    .scale(x)
@@ -20,11 +22,17 @@ function scatterPlot() {
 	    .scale(y)
 	    .orient("left");
 
+	// graph canvas
 	var svg = d3.select("body").append("svg")
 	    .attr("width", width + margin.left + margin.right)
 	    .attr("height", height + margin.top + margin.bottom)
 	  .append("g")
 	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	// tooltip area to the webpage
+	var tooltip = d3.select("body").append("div")
+	    .attr("class", "tooltip")
+	    .style("opacity", 0);
 
 	d3.csv("top-websites.csv", function(error, data) {
 	  if (error) throw error;
@@ -32,11 +40,25 @@ function scatterPlot() {
 	  data.forEach(function(d) {
 	    d.unique_visitors = +d.unique_visitors;
 	    d.pageviews = +d.pageviews;
+
+	    // Organize categories
+		var category = d.category.split('-');
+		if (category[0].substring(category[0].length/2 + 1) === category[0].substring(0, category[0].length/2)) {
+			d.main_category = category[0].substring(category[0].length/2 + 1);
+		} else {
+			d.main_category = category[0];
+		}
+		d.sub_category = category[1];
+
+		if (d.main_category === "") {
+			d.main_category = "Undefined";
+		}
 	  });
 
 	  x.domain(d3.extent(data, function(d) { return d.pageviews; })).nice();
 	  y.domain(d3.extent(data, function(d) { return d.unique_visitors; })).nice();
 
+	  // x-axis
 	  svg.append("g")
 	      .attr("class", "x axis")
 	      .attr("transform", "translate(0," + height + ")")
@@ -48,6 +70,7 @@ function scatterPlot() {
 	      .style("text-anchor", "end")
 	      .text("Page Views (U.S.)");
 
+	  // y-axis
 	  svg.append("g")
 	      .attr("class", "y axis")
 	      .call(yAxis)
@@ -59,12 +82,51 @@ function scatterPlot() {
 	      .style("text-anchor", "end")
 	      .text("Unique Visitors (U.S.)")
 
+	  // draw dots
 	  svg.selectAll(".dot")
 	      .data(data)
 	    .enter().append("circle")
 	      .attr("class", "dot")
 	      .attr("r", 3.5)
 	      .attr("cx", function(d) { return x(d.pageviews); })
-	      .attr("cy", function(d) { return y(d.unique_visitors); });
+	      .attr("cy", function(d) { return y(d.unique_visitors); })
+	      .style("fill", function(d) { return color(cValue(d));})
+	      .on("mouseover", function(d) {
+          tooltip.transition()
+               .duration(200)
+               .style("opacity", .9);
+          tooltip.html(d.site + "<br>" +
+          			   d.main_category)
+               .style("left", (d3.event.pageX + 5) + "px")
+               .style("top", (d3.event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+          tooltip.transition()
+               .duration(500)
+               .style("opacity", 0);
+      });;
+	  
+	  // draw legend
+	  var legend = svg.selectAll(".legend")
+	      .data(color.domain())
+	    .enter().append("g")
+	      .attr("class", "legend")
+	      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+	  // draw legend colored rectangles
+	  legend.append("rect")
+	      .attr("x", width - 18)
+	      .attr("width", 18)
+	      .attr("height", 18)
+	      .style("fill", color);
+
+	  // draw legend text
+	  legend.append("text")
+	      .attr("x", width - 24)
+	      .attr("y", 9)
+	      .attr("dy", ".35em")
+	      .style("text-anchor", "end")
+	      .text(function(d) { return d;})
+
 	});
 }
