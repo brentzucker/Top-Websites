8561e12ffd9b9504b33347c67b92a40d46d88bae
brentzucker <brentzucker@gmail.com>
2015-11-10 11:32:50 -0500

    removed global margins

--- main.js
+++ main.js
@@ -4,6 +4,29 @@ $(function() {
 	    width = 960 - margin.left - margin.right,
 	    height = 500 - margin.top - margin.bottom;
 
+
+	// Fill Color
+	cValue = function(d) { 
+			if ('key' in d) {
+				return d.key; 
+			} else if ('main_category' in d) {
+				return d.main_category;
+			}
+		},
+    color = d3.scale.category20();
+
 	scatterPlot();
 	barChart();
+});
+
+$(function() {
+	$( "#slider-range" ).slider({
+		range: true,
+		min: 0,
+		max: 1000,
+		values: [ 0, 1000 ],
+		slide: function( event, ui ) {
+			// $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+		}
+	});
 });
