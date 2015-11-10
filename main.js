dc69e44130d79bdcf731ee1764c31a64624819e5
brentzucker <brentzucker@gmail.com>
2015-11-10 11:33:08 -0500

    removed global margins

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 $(function() {
 	
-	margin = {top: 20, right: 80, bottom: 30, left: 80},
-	    width = 960 - margin.left - margin.right,
-	    height = 500 - margin.top - margin.bottom;
 
 
 	// Fill Color
