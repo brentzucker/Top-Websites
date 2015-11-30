2bd4effc69a36fa6403af170df402b2c60436c7e
brentzucker <brentzucker@gmail.com>
2015-11-30 15:05:45 -0500

    search works

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ $(function() {
 		},
     color = d3.scale.category20();
 
+  searchBar();
 	scatterPlot(1, 1000);
 	barChart(1, 1000);
 	drawSlider();
@@ -18,7 +19,7 @@ $(function() {
 function drawSlider() {
 
 	// Create Slider
-    $( "#slider-range" ).slider({
+    $("#slider-range").slider({
       range: true,
       min: 1,
       max: 1000,
@@ -31,13 +32,11 @@ function drawSlider() {
         $('#left_corner').html(rank['min']);
         $('#right_corner').html(rank['max']);
 
-        // console.log(rank);
-
         // update scatter plot
         updateScatterPlot(rank['min'], rank['max']);
 
-    	 // update bar chart
-    	 updateBarChart(rank['min'], rank['max']);
+        // update bar chart
+        updateBarChart(rank['min'], rank['max']);
       }
     });
 
@@ -54,4 +53,36 @@ function drawSlider() {
     // Initialize corners with default values
     $('#left_corner').html($( "#slider-range" ).slider('values', 0));
     $('#right_corner').html($( "#slider-range" ).slider('values', 1));
+}
+
+function searchBar() {
+
+  // Get array of all website names
+  var website_names = [];
+  d3.csv("top-websites.csv", function(error, data) {
+
+    data.forEach(function(d) {
+      website_names.push(d.site);
+    });
+  });
+
+  $('#search').on('change keyup paste', function() {
+    
+    // console.log(website_names);
+    var search_term = $('#search').val();
+
+    // Is term in any website name?
+    var website = getWebsiteName(website_names, search_term);
+    console.log(website);
+    
+    // $('#' + website).attr('class', 'search-result');
+    document.getElementById(website).setAttribute('class', 'dot search-result');
+  });
+}
+
+function getWebsiteName(website_names, search_term) {
+  
+  // If search term is the exact name of website
+  if (website_names.indexOf(search_term) > -1) return website_names[website_names.indexOf(search_term)];
+  return undefined;
 }
